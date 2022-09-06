1a072e8f0cfba0b109fca458aafd0d7a8804bc96
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2022-09-06 15:26:14 +0600

    asyncawait  & DOM

---
+++ async-await.js
@@ -0,0 +1,89 @@
+// const abc = false;
+// console.log("Now, I am learning Promise");
+
+// //promise definition
+// const promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     if (abc) {
+//       resolve("Success");
+//     } else {
+//       reject("failed");
+//     }
+//   }, 2000);
+// });
+
+// //promise call
+
+// promise
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+// console.log("Task end");
+
+const paymentSucced = true;
+const mark = 80;
+
+function enroll() {
+  console.log("Course enrollment is in progress");
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (paymentSucced) {
+        resolve();
+      } else {
+        reject("Payment failed");
+      }
+    }, 2000);
+  });
+  return promise;
+}
+
+function progress() {
+  console.log("Course on pregress.....");
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (mark >= 80) {
+        resolve();
+      } else {
+        reject("You could not get enought mark to get the Certificate");
+      }
+    }, 3000);
+  });
+  return promise;
+}
+
+function getCertificate() {
+  console.log("Please, wait... Preparing your certificate ");
+  const promise = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("Congrats! You got the certificate");
+    }, 1000);
+  });
+  return promise;
+}
+
+async function course() {
+  try {
+    await enroll();
+    await progress();
+    const message = await getCertificate();
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+  }
+}
+course();
+
+// enroll()
+//   .then(progress)
+//   .then(getCertificate)
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
---
+++ domScript.js
@@ -0,0 +1,20 @@
+// const p = document.getElementById("demo");
+// // console.log(p);
+// p.innerHTML = "Test 1";
+
+// const element = document.getElementById("myImage");
+// element.src = "landscape.jpg";
+
+// const date = document.getElementById("date");
+// date.innerHTML = `Today's date: ${Date()}`;
+// console.log(date);
+
+function validateForm() {
+  const form = document.forms["myForm"];
+  //   console.log(form);
+  const value = form["fname"].value;
+  if (value == "") {
+    alert("You must be provide a name");
+    return false;
+  }
+}
