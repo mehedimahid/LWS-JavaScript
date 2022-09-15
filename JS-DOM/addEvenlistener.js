9ed69f8297d90ee516635314c63c488cf303e9cf
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2022-09-15 12:16:06 +0600

    JS DOM

--- JS-DOM/addEvenlistener.js
+++ JS-DOM/addEvenlistener.js
@@ -1,14 +1,38 @@
-const style = document.querySelector("#style");
+// const style = document.querySelector("#style");
 
-function myfunction(text) {
-  console.log(text);
-}
+// function myfunction(text) {
+//   console.log(text);
+// }
 
 // style.addEventListener("click", myfunction);
 //when need parameter passing
-style.addEventListener("click", function () {
-  myfunction("button clicked");
-});
-style.addEventListener("mouseover", function () {
-  console.log("use put his mouse over this element");
-});
+// style.addEventListener("click", function () {
+//   myfunction("button clicked");
+//   // gghfgfg
+// });
+
+// style.addEventListener("mouseover", function () {
+//   console.log("use put his mouse over this element");
+// });
+
+const myDiv = document.getElementById("myDIV");
+
+myDiv.addEventListener("mousemove", myMoveFunction);
+
+function myMoveFunction() {
+  document.getElementById("demo").innerHTML = Math.random();
+}
+
+function removeHandler() {
+  myDiv.removeEventListener("mousemove", myMoveFunction);
+}
+
+// document.getElementById("myDIV").addEventListener("mousemove", myFunction);
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = Math.random();
+// }
+
+// function removeHandler() {
+//   document.getElementById("myDIV").removeEventListener("mousemove", myFunction);
+// }
---
+++ JS-DOM/navigation/navigation.js
@@ -0,0 +1,11 @@
+// const p = document.getElementById("demo");
+
+// const way1 = p.innerHTML;
+// const way2 = p.firstChild.nodeValue;
+
+// const way3 = p.childNodes[0].nodeValue;
+// console.log(way1); //return text
+// console.log(way2); //return text
+// console.log(way3); //return text
+
+//
